d7454f2053545ea130cd68fa2a3160a3e755a741
MomoDevlop <savadhammed@gmail.com>
2020-12-28 19:52:13 +0000

    webpack integration

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path')
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, 'public/')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function (req, res) {
-	res.sendFile(path.join(__dirname, '/dist/index.html'))
+	res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
 const port = process.env.PORT || 8080
